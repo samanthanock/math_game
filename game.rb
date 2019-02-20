1c88135dc0c0124979b7ad1300713272f790fb34
samanthamarienock <sammarienock@gmail.com>
2019-02-19 19:51:05 -0800

    realized need to initiate a game turn file, creates game play method

--- game.rb
+++ game.rb
@@ -18,3 +18,17 @@ module Math
 
     #need to show 1. live players 2. turns 3. end, what happens when someone loses?
 
+def begin
+  until @players.length === 1 do
+    @players.each.with_index do |players, index|
+    #what is the difference between .with_index and .each_with_index?
+    ## stackoverflow answer: The with_index method takes an optional parameter to offset the starting index. each_with_index does the same thing, but has no optional starting index.
+turn = Turn.new(player, Question.new)
+turn.start
+
+puts self.score(current_players)
+#self gives you access to the current object -- the object that is recieving the currenty message.
+winner = current_players
+  puts "#{winner.name} ---you won---game over---"
+end
+end
